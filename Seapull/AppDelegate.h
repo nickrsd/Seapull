a0f1c5fd8bc82c62d91033edd5236192a47cfadb
Dominic <nickrsd@gmail.com>
2017-08-12 19:17:53 -0700

    Initial Commit

---
+++ Seapull/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Seapull
+//
+//  Created by Dominic Rodriquez on 8/12/17.
+//  Copyright © 2017 RamblingTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Seapull/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seapull
+//
+//  Created by Dominic Rodriquez on 8/12/17.
+//  Copyright © 2017 RamblingTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
