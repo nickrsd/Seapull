1a12fd341e49c6de0cd280e36b74e67069622939
Dominic <nickrsd@gmail.com>
2017-08-13 21:59:29 -0700

    Skeleton Objective-C app setup wtih Cocoapods, an empty Swift bridging header, and a placeholder app icon.

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Seapull' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Seapull
+  pod 'ReSwift'
+
+  target 'SeapullTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SeapullUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Seapull/SPDashboardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SPDashboardViewController.swift
+//  Seapull
+//
+//  Created by Dominic Rodriquez on 8/13/17.
+//  Copyright © 2017 RamblingTree. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+public class SPDashboardViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
