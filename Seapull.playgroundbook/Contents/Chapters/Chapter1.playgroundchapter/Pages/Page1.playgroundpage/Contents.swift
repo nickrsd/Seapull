7eaecc6f0163fb6246c03a4e4ab46b1d0be371b4
Dominic <nickrsd@gmail.com>
2017-08-14 00:03:39 -0700

    added barebones diffViewController and a swift file for the first playground page that hooks it up to the live view

---
+++ Seapull.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+*/
+
+/* let selectableNum = /*#-editable-code*/<#number of llamas#>/*#-end-editable-code*/ */
+
+//#-hidden-code //#=end-hidden-code
+/*#-editable-code placeholder text*/ /*#-end-editable-code*/
+ 
+ 
+
+//#-editable-code Tap to start coding
+import UIKit
+import PlaygroundSupport
+
+let diffViewController = SPDiffViewController()
+PlaygroundPage.current.liveView = diffViewController
